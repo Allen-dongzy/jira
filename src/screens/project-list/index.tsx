6accc3098302e6cd8108de9d52a47d461d9fe1b3
dongzhengyang <allen.dongzy@gmail.com>
2021-08-26 18:37:02 +0800

    build: modify prettier rule

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,50 +1,51 @@
-import { SearchPanel } from "./search-panel";
-import { List } from "./list";
-import { useEffect, useState } from "react";
-import { cleanObject, useDebounce, useMount } from "../../utils";
-import qs from "qs";
+import { SearchPanel } from "./search-panel"
+import { List } from "./list"
+import { useEffect, useState } from "react"
+import { cleanObject, useDebounce, useMount } from "../../utils"
+import qs from "qs"
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.REACT_APP_API_URL
 
 export interface Projects {
-  id: number;
-  name: string;
-  personId: number;
-  organization: string;
-  created: number;
+  id: number
+  name: string
+  personId: number
+  organization: string
+  created: number
 }
 
 export interface Users {
-  id: number;
-  name: string;
+  id: number
+  name: string
 }
+
 export const ProjectListScreen = () => {
   const [params, setParam] = useState({
     name: "",
     personId: "",
-  });
-  const debounceParams = useDebounce(params);
-  const [projects, setProjects] = useState([]);
-  const [users, setUsers] = useState([]);
+  })
+  const debounceParams = useDebounce(params)
+  const [projects, setProjects] = useState([])
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
     fetch(
       `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParams))}`
     ).then(async (res) => {
-      if (res.ok) setProjects(await res.json());
-    });
-  }, [debounceParams]);
+      if (res.ok) setProjects(await res.json())
+    })
+  }, [debounceParams])
 
   useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
-      if (res.ok) setUsers(await res.json());
-    });
-  });
+      if (res.ok) setUsers(await res.json())
+    })
+  })
 
   return (
     <div>
       <SearchPanel params={params} setParam={setParam} users={users} />
       <List projects={projects} users={users} />
     </div>
-  );
-};
+  )
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,38 +1,38 @@
 // 判断是否为false
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 export const isFlasy = (value: unknown): boolean =>
-  value === 0 ? false : !value;
+  value === 0 ? false : !value
 
 // 清空对象中无效的属性
 export const cleanObject = (obj: object): object => {
-  const result = { ...obj };
+  const result = { ...obj }
   // @ts-ignore
-  Object.keys(result).forEach((key) => isFlasy(obj[key]) && delete result[key]);
-  return result;
-};
+  Object.keys(result).forEach((key) => isFlasy(obj[key]) && delete result[key])
+  return result
+}
 
 // 自定义hook-执行一次的useEffect
 export const useMount = (callback: () => void): void => {
   useEffect(() => {
-    callback();
-  }, []);
-};
+    callback()
+  }, [])
+}
 
 // 自定义hook-防抖
 export const useDebounce = <T>(value: T, delay = 500): T => {
   // 定义debounceValue，值为value
-  const [debounceValue, setDebounceValue] = useState(value);
+  const [debounceValue, setDebounceValue] = useState(value)
   //使用useEffect监听value和delay
   useEffect(() => {
     // 每次value或delay变化后都设置一个定时器
     const timer = setTimeout(() => {
       // 若是顺利延时则设置debounceValue为最新的value
-      setDebounceValue(value);
-    }, delay);
+      setDebounceValue(value)
+    }, delay)
     // 清理上一个useEffect中的timer
-    return () => clearTimeout(timer);
-  }, [value, delay]);
+    return () => clearTimeout(timer)
+  }, [value, delay])
   //返回debounceValue(最新的value)
-  return debounceValue;
-};
+  return debounceValue
+}
